eb30a099c8724f068ebb8877a62a905a514228fc
Leonora Squires <leonora.squires@gmail.com>
2020-04-24 16:31:41 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,7 +2,7 @@ def find_min_value(array)
   count = 0
   min = array[count]
   while count < array[count] do
-    if min > array[count]
+    if min >= array[count]
       min = array[count]
     end
     count+=1
